282586861162267bba85017a32a3256535e4a27c
KAKATb-CTO9I <121755078+KAKATb-CTO9I@users.noreply.github.com>
2023-05-26 15:18:15 +0300

    committ

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,19 @@
 import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+import {connect, useDispatch} from "react-redux";
 import BackendService from '../services/BackendService';
 import Utils from "../utils/Utils";
-//import {connect} from "react-redux"
-import {useNavigate} from "react-router-dom";
+import {userActions} from "../utils/Rdx";
 
-export default function  Login() {
+
+export default connect() (function  Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [loggingIn, setLoggingIn] = useState(false);
     const [submitted, setSubmitted] = useState(false);
-    const [error_message, setErrorMessage] = useState(null);
+    // const [error_message, setErrorMessage] = useState(null);
     const nav = useNavigate();
+    const dispatch = useDispatch();
 
     function handleChangeLogin(e) {
         setUsername(e.target.value);
@@ -23,28 +26,29 @@ export default function  Login() {
     function handleSubmit(e) {
         e.preventDefault();
         setSubmitted(true);
-        setErrorMessage(null);
+        // setErrorMessage(null);
         setLoggingIn(true);
         BackendService.login(username, password)
             .then ( resp => {
                 console.log(resp.data);
+                dispatch(userActions.login(resp.data));
                 Utils.saveUser(resp.data);
                 setLoggingIn(false);
                 nav("/home");
             })
             .catch( err => {
-                if (err.response && err.response.status === 401)
-                    setErrorMessage("Ошибка авторизации");
-                else
-                    setErrorMessage(err.message);
+                // if (err.response && err.response.status === 401)
+                //     setErrorMessage("Ошибка авторизации");
+                // else
+                //     setErrorMessage(err.message);
                 setLoggingIn(false);
             })
     }
 
     return  (
         <div className="col-md-6 me-0">
-            {error_message &&
-                <div className="alert alert-danger mt-1 me-0 ms-0">{error_message}</div>}
+            {/*{error_message &&*/}
+            {/*    <div className="alert alert-danger mt-1 me-0 ms-0">{error_message}</div>}*/}
             <h2>Вход</h2>
             <form name="form" onSubmit={handleSubmit}>
                 <div className="form-group">
@@ -72,5 +76,4 @@ export default function  Login() {
             </form>
         </div>
     );
-}
-//export default connect()(Login);
+})
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,46 +1,57 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
-import { useNavigate , Link} from 'react-router-dom';
+import {faBars, faUser} from '@fortawesome/free-solid-svg-icons'
+import {Link, useNavigate} from 'react-router-dom';
 import Utils from "../utils/Utils";
 import BackendService from "../services/BackendService";
+import {userActions} from "../utils/Rdx";
+import {connect} from "react-redux";
 
 class NavigationBarClass extends React.Component {
 
     constructor(props) {
         super(props);
         this.goHome = this.goHome.bind(this);
-    }
-    logout() {
-        BackendService.logout().then(() => {
-        Utils.removeUser();
-        this.goHome();
-        });
+        this.logout = this.logout.bind(this);
     }
 
     goHome() {
         this.props.navigate('Home');
     }
+
+    /* logout() {
+         BackendService.logout().then(() => {
+             Utils.removeUser();
+             this.props.dispatch(userActions.logout())
+             this.props.navigate('Login');
+         });
+     }
+     */
+    logout() {
+        Utils.removeUser();
+        this.props.dispatch(userActions.logout())
+        this.props.navigate('Login');
+    }
+
     render() {
-        let uname = Utils.getUserName();
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}Foma RPO</Navbar.Brand>
+                <button type="button"
+                        className="btn btn-outline-secondary mr-2"
+                        onClick={this.props.toggleSideBar}>
+                    <FontAwesomeIcon icon={ faBars} />
+                </button>
+                <Navbar.Brand>My RPO</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link onClick={this.goHome}>Another Home</Nav.Link>
-                        <Nav.Link onClick={() =>{ this.props.navigate("\home")}}>Yet Another Home</Nav.Link>
+                        <Nav.Link onClick={() =>{ this.props.navigate("/home")}}>Another Home</Nav.Link>
                     </Nav>
-                    <Navbar.Text>{uname}</Navbar.Text>
-                    { uname &&
-                        <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>
-                    }
-                    { !uname &&
-                        <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>
-                    }
+                    <Navbar.Text>{this.props.user && this.props.user.login}</Navbar.Text>
+                    {this.props.user && <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>}
+                    {!this.props.user && <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>}
                 </Navbar.Collapse>
             </Navbar>
         );
@@ -52,4 +63,9 @@ const NavigationBar = props => {
 
     return <NavigationBarClass navigate={navigate} {...props} />
 }
-export default  NavigationBar;
+
+const mapStateToProps = state => {
+    const { user } = state.authentication;
+    return { user };
+}
+export default connect(mapStateToProps)(NavigationBar);
